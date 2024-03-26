2e9b0eb78e2cb412a385023a3186932b877682ca
Cameron <cfrue@unca.edu>
2024-03-26 12:52:28 -0400

    Project and Lectures

---
+++ lectures/lecture16/02-function-with-parameters/main.js
@@ -0,0 +1,15 @@
+// make a function:
+function changeColor(color){
+    const bodyeEl= document.querySelector("body");
+    bodyeEl.style.backgroundColor = color;
+}
+// ^^^^Same thing as what is greened out put way easier
+// function makeRed() {
+// document.querySelector("body").style.backgroundColor ="#B23";
+// }
+// function makeBlue() {
+//     document.querySelector("body").style.backgroundColor ="#2af";
+// }
+// function makeGreen() {
+//     document.querySelector("body").style.backgroundColor ="#3a3";
+// }
