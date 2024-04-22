e9460f632f5343e4254980975692946ed1f40705
Cameron <cfrue@unca.edu>
2024-04-22 13:11:25 -0400

    Tutorial 11

--- lectures/lecture20/main.js
+++ lectures/lecture20/main.js
@@ -1,7 +1,8 @@
 // ignore this function for now. We'll go over it
 // on Wednesday: 
+let data;
 async function fetchCourses() {
-    const url = `https://meteor.unca.edu/registrar/class-schedules/api/v1/courses/2023/fall/`;
+    const url = `https://meteor.unca.edu/registrar/class-schedules/api/v1/courses/2024/fall/`;
     data = await fetch(url).then(response => response.json());
 } 
 fetchCourses();
@@ -27,4 +28,9 @@ fetchCourses();
 function search() {
     console.log(data);
     const searchTerm = document.querySelector('#search_term').value;
+    for(let i=0; i < data.length; i++){
+        if (data[i].Department===searchTerm){
+    console.log(data[i].Title);
+        }
+}
 }
