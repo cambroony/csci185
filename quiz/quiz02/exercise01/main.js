925612c3f664589a57c4780d34a7da7ae5e9ea59
Cameron <cfrue@unca.edu>
2024-05-03 13:54:32 -0400

    Final Exam makeup

--- quiz/quiz02/exercise01/main.js
+++ quiz/quiz02/exercise01/main.js
@@ -1,12 +1,12 @@
 function changeBackground() {
     // your code here
-    document.querySelector("#s1").style.backgroundColor = "blue"
+    document.querySelector("#s1").style.backgroundColor = "blue";
 /// What is above is the same thing as what is below,
 //the differenece between them is just being seperated into two lines
 //1. select the element
     document.querySelector("#s1")
 //2. change it
-    el.style.backgroundColor = "blue"
+    el.style.backgroundColor = "blue";
 }
 
 function goodbye() {
